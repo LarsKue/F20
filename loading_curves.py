386acf7a2430f36649774473f1ff0e846941aaf1
Impfkristall <lorenz.hahn@outlook.de>
2019-10-22 18:01:13 +0200

    for review

--- loading_curves.py
+++ loading_curves.py
@@ -9,7 +9,6 @@ from scipy import constants as consts
 from scipy.optimize import curve_fit
 from scipy.stats import chi2
 
-
 data_folder = "data/loading_curves/"
 
 
@@ -32,41 +31,83 @@ def get_all_data():
         i += 1
 
 
-def conv_volts_to_atomnumber():
+data_detuning = "data/detuning_coil_curves/"
 
+
+def get_detuning_data(filename_detuning, skip_rows=18, separator=","):
+    with open(filename_detuning, "r") as f:
+        for linenum, line in enumerate(f):
+            if linenum < skip_rows:
+                continue
+            yield tuple(float(x) for x in line.strip().split(separator) if x)
+
+
+def get_all_detuning_data():
+    i = 4
+    while True:
+        filename_detuning = data_detuning + "F{:04d}CH1.csv".format(i)
+        try:
+            yield zip(*list(get_detuning_data(filename_detuning)))
+        except FileNotFoundError:
+            break
+        i += 1
+
+
+def conv_volts_to_atomnumber():
     def intens_0(p_powermeter):
         return (2 * p_powermeter) / (np.pi * ((2E-3) ** 2))
 
-    def gamma_sc(delta):
-        gamma = 2*np.pi*6.07E6
-        I_sat = 4.1 #milivolts per squarecentimetre
-        return (gamma/2) * ((intens_0(p_powermeter)/I_sat)/(1+(intens_0(p_powermeter)/I_sat) + 4 * ((delta ** 2)/gamma ** 2)))
+    def gamma_sc(delta, p_powermeter):
+        gamma = 2 * np.pi * 6.07E6
+        I_sat = 4.1  # milivolts per squarecentimetre
+        return (gamma / 2) * ((intens_0(p_powermeter) / I_sat) / (
+                    1 + (intens_0(p_powermeter) / I_sat) + 4 * ((delta ** 2) / gamma ** 2)))
 
-    def wavelength_to_energy(lambda):
-        return 6.62607015E-34 * 299792458 / lambda
+    def wavelength_to_energy(l):
+        return 6.62607015E-34 * 299792458 / l
 
-    def conversion_to_atoms(V_out):
+    def conversion_to_atoms(V_out, delta, E_nu):
         S = 1E6 / (1E6 + 50)
         T = 0.96
-        G = ufloat(4.75E6,4.75E6 * 0.05) # Volts/Ampere
-        QE ufloat(0.52,0.015) # Ampere/Watt
-        theta_omega = (np.pi * ((25.4E-3) ** 2))/(4*np.pi*((150E-3) ** 2))
+        G = ufloat(4.75E6, 4.75E6 * 0.05)  # Volts/Ampere
+        QE = ufloat(0.52, 0.015)  # Ampere/Watt
+        theta_omega = (np.pi * (25.4E-3 ** 2)) / (4 * np.pi * (150E-3 ** 2))
 
         return V_out / (QE * G * S * T * theta_omega * gamma_sc(delta) * E_nu)
 
+    def detuning_calculator(x):
+        return  2*x -60 - 2*85
+
+
+    detunings = detuning_calculator(np.array([109.75, 110.25, 110.75, 111.25, 111.75, 112.25]))
+    print("hier", detuning_calculator(detunings))
+
     return
 
-
+conv_volts_to_atomnumber()
 def main(argv: list) -> int:
     print(list(get_data(data_folder + "F0004CH1.CSV")))
 
-    for xdata, ydata in get_all_data():
-        plt.plot(xdata, ydata)
+    # for xdata, ydata in get_all_data():
+    #     plt.plot(xdata, ydata)
+    #     plt.show()
+    #
+    #     print(xdata)
+    #     print(ydata)
+    #     print()
+
+    for xdata, ydata in get_all_detuning_data():
+        x, y = zip(*list(get_detuning_data("data/detuning_coil_curves/F0028CH1.CSV")))
+
+        y = np.array(y)
+        y = np.where(y < 0.07, 0.0849, y)
+
+        xdata = np.array(xdata)
+        ydata = np.array(ydata)
+
+        plt.plot(xdata, ydata - y)
         plt.show()
 
-        print(xdata)
-        print(ydata)
-        print()
     return 0
 
 
