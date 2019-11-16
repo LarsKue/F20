Finished dsolve, Need start values added nicer data masking, fixed bugs with dsolve, now works as intended, added vertical lines in plot to show which roots were found, function needs improved starting values (i.e. non-generated) to find all roots

spectroscopy.py
@@ -46,23 +46,6 @@ def u_to_kg(m):
     return 1.66053904e-27 * m
 
 
-def mask_data(mask: Callable[[List], List], keyarr: List, *data: List[List], modify_keyarr: bool = True,
-              output_type_modifier=None):
-    m: List = mask(keyarr)
-    if modify_keyarr:
-        result = (x for i, x in enumerate(keyarr) if m[i])
-        if output_type_modifier is None:
-            yield type(keyarr)(result)
-        else:
-            yield output_type_modifier(result)
-    for arr in data:
-        result = (x for i, x in enumerate(arr) if m[i])
-        if output_type_modifier is None:
-            yield type(arr)(result)
-        else:
-            yield output_type_modifier(result)
-
-
 def get_data(filename: str):
     with open(filename, "r") as f:
         for i, line in enumerate(f):
@@ -278,21 +261,15 @@ def lorentzfit(lorentz_data, plot=True, return_gammas=False):
 
 
 def hyperfine(plot=True):
-    # data = list(get_hyperfine_data(plot=False))
 
     for data_out, data_in, pdh in get_hyperfine_data(plot=False):
-        mean_N = 30
-        smooth_pdh = rolling_mean(pdh, mean_N)
-
+        mean_N = 20
+        smooth_pdh = np.array(rolling_mean(pdh, mean_N))
 
         # find solutions for phd
-        print(list(closest_indices(data_out[mean_N // 2:-mean_N // 2 + 1], *np.linspace(-0.2, 0.2, 11))))
-        print("-----")
-        peaks = dsolve(data_out[mean_N // 2:-mean_N // 2 + 1], smooth_pdh, x0=np.linspace(-0.2, 0.2, 11))
+        peaks = dsolve(np.array(data_out[mean_N // 2:-mean_N // 2 + 1]), smooth_pdh, x0=np.linspace(min(data_out) + 0.1 * abs(min(data_out)), max(data_out) - 0.1 * abs(max(data_out)), 11))
 
-        print(peaks)
-
-
+        print("peaks:", sorted(peaks))
 
         fig, ax1 = plt.subplots(figsize=(10, 8))
         color = "tab:blue"
@@ -303,6 +280,9 @@ def hyperfine(plot=True):
 
         ax2 = ax1.twinx()
         ax2.axhline(color="red")
+
+        for x0 in peaks:
+            plt.axvline(x=x0)
 
         color = "tab:green"
         ax2.set_ylabel("PDH [a.u.]", color=color)

utils.py
@@ -1,5 +1,5 @@
 
-from typing import List, Iterable, Sized
+from typing import List, Iterable, Sized, Callable, Sequence
 
 from scipy.optimize import fsolve
 from math import isclose
@@ -11,15 +11,43 @@ def modify_data(modifier, *data):
         yield type(d)(modifier(x) for x in d)
 
 
+def mask_data(mask: Callable[[Iterable], List], keyarr: Iterable, *data: List[Iterable], modify_keyarr: bool = True,
+              output_type_modifier: Callable = None):
+    m: List = mask(keyarr)
+    if modify_keyarr:
+        result = (x for i, x in enumerate(keyarr) if m[i])
+        if output_type_modifier is None:
+            yield type(keyarr)(result)
+        else:
+            yield output_type_modifier(result)
+    for arr in data:
+        result = (x for i, x in enumerate(arr) if m[i])
+        if output_type_modifier is None:
+            yield type(arr)(result)
+        else:
+            yield output_type_modifier(result)
+
+
+def index_mask_data(mask: Sequence, keyarr: Sequence, *data: List[Sequence], modify_keyarr: bool = True,
+                    output_type_modifier: Callable = None):
+    if modify_keyarr:
+        result = (keyarr[i] for i in mask)
+        if output_type_modifier is None:
+            yield type(keyarr)(result)
+        else:
+            yield output_type_modifier(result)
+        for arr in data:
+            result = (arr[i] for i in mask)
+            if output_type_modifier is None:
+                yield type(arr)(result)
+            else:
+                yield output_type_modifier(result)
+
+
 def closest_index(data: [Iterable, Sized], value):
     """ Find the index of the closest data point to value in the data set """
     data = sorted(data)
     return min(range(len(data)), key=lambda i: abs(data[i] - value))
-
-
-def closest_indices(data: [Iterable, Sized], *values):
-    for value in values:
-        yield closest_index(data, value)
 
 
 def sort_together(keyarr: [Iterable, Sized], *arrs) -> List:
@@ -32,39 +60,47 @@ def sort_together(keyarr: [Iterable, Sized], *arrs) -> List:
 
 def dsolve(xdata, ydata, *args, **kwargs):
     """ Solve a given dataset for 0 by interleaving the data points with linear functions"""
-    # need x to be sorted for interleaving
+    # need x to be sorted (monotonically increasing) for interleaving
     xdata, ydata = sort_together(xdata, ydata)
 
     # helper function that connects data points with a linear function
     def f(x):
         if isinstance(x, Iterable):
-            # FIXME: idx comes out empty or all zeroes
-            print(*x)
-            print(type(x))
-            idx = type(x)(list(closest_indices(xdata, *x)))
-            print(idx)
-            print(type(idx))
-        else:
-            idx = closest_index(xdata, x)
+            return [f(y) for y in x]
 
-        print(idx)
-        print(type(idx))
+        idx = closest_index(xdata, x)
         x1, y1 = xdata[idx], ydata[idx]
 
-        if x >= x1 or idx == 0:
+        if x >= x1 and idx != len(xdata) - 1 or idx == 0:
             # connect to the right
-            x2, y2 = xdata[idx + 1], ydata[idx + 1]
+            idx += 1
+            x2, y2 = xdata[idx], ydata[idx]
         else:
             # connect to the left
-            x2, y2 = xdata[idx - 1], ydata[idx - 1]
+            idx -= 1
+            x2, y2 = xdata[idx], ydata[idx]
 
             # swap the points so they are in order from left to right
             x1, x2 = x2, x1
             y1, y2 = y2, y1
 
-        if isclose(x1, x2):
+        while isclose(x1, x2):
             # x1 and x2 must not be the same since we are dividing by their difference later
-            raise RuntimeError("dsolve cannot interleave datasets with duplicate xdata points")
+            # so go to the right or left even more to find an appropriate data point
+            if x >= x1:
+                # connect further to the right
+                if idx == len(xdata) - 1:
+                    # no more data, assume constant
+                    return y1
+                idx += 1
+                x2, y2 = xdata[idx], ydata[idx]
+            else:
+                # connect further to the left
+                if idx == 0:
+                    # no more data, assume constant
+                    return y1
+                idx -= 1
+                x2, y2 = xdata[idx], ydata[idx]
 
         # f(x) = dfdx * x + f0
         dfdx = (y2 - y1) / (x2 - x1)